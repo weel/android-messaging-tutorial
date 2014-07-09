eecc5ac7c0767bbfaa76d8c6e3774c4af9fc3886
Jordan Hamel <jo.mravca@gmail.com>
2014-07-09 13:11:49 -0700

    Add Parse libs and credentials

--- MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/LoginActivity.java
+++ MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/LoginActivity.java
@@ -2,6 +2,7 @@ package com.sinch.messagingtutorial.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.parse.Parse;
 
 public class LoginActivity extends Activity {
 
@@ -9,5 +10,7 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Parse.initialize(this, "IhUPJfNhqBPLns6dXn6BeW3BMfGOlcRElMoYnilM", "1tTdMRSmK74ZzZtYBAPCJFoSbahBxi8cNt6TYj9U");
     }
 }
