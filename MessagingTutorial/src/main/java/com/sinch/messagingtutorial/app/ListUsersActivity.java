7da10a734d57b6d4ceca2885080f63efdb71fcf3
Jordan Hamel <jo.mravca@gmail.com>
2014-07-10 16:49:20 -0700

    Create and populate user list

--- MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/ListUsersActivity.java
+++ MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/ListUsersActivity.java
@@ -2,12 +2,60 @@ package com.sinch.messagingtutorial.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListUsersActivity extends Activity {
+
+    private String currentUserId;
+    private ArrayAdapter<String> namesArrayAdapter;
+    private ArrayList<String> names;
+    private ListView usersListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_users);
+
+        currentUserId = ParseUser.getCurrentUser().getObjectId();
+
+        setConversationsList();
+    }
+
+    private void setConversationsList() {
+
+        names = new ArrayList<String>();
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereNotEqualTo("objectId", currentUserId);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            public void done(List<ParseUser> userList, ParseException e) {
+                if (e == null) {
+                    for (int i=0; i<userList.size(); i++) {
+                        names.add(userList.get(i).getUsername().toString());
+                    }
+
+                    usersListView = (ListView)findViewById(R.id.usersListView);
+                    namesArrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                        R.layout.user_list_item, names);
+                    usersListView.setAdapter(namesArrayAdapter);
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                        "Error loading user list",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
