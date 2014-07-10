cb8b9a1e9d07003bd1cfdb342fe23f16e3591efc
Jordan Hamel <jo.mravca@gmail.com>
2014-07-10 15:46:04 -0700

    Create ListUsersActivity

---
+++ MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/ListUsersActivity.java
@@ -0,0 +1,13 @@
+package com.sinch.messagingtutorial.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListUsersActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_users);
+    }
+}
--- MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/LoginActivity.java
+++ MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/LoginActivity.java
@@ -1,16 +1,94 @@
 package com.sinch.messagingtutorial.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.parse.LogInCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+
+    private Button signUpButton;
+    private Button loginButton;
+    private EditText usernameField;
+    private EditText passwordField;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
         Parse.initialize(this, "IhUPJfNhqBPLns6dXn6BeW3BMfGOlcRElMoYnilM", "1tTdMRSmK74ZzZtYBAPCJFoSbahBxi8cNt6TYj9U");
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), ListUsersActivity.class);
+            startActivity(intent);
+        }
+
+        setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signupButton);
+        usernameField = (EditText) findViewById(R.id.loginUsername);
+        passwordField = (EditText) findViewById(R.id.loginPassword);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = usernameField.getText().toString();
+                password = passwordField.getText().toString();
+
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Intent intent = new Intent(getApplicationContext(), ListUsersActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                "Wrong username/password combo",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                username = usernameField.getText().toString();
+                password = passwordField.getText().toString();
+
+                ParseUser user = new ParseUser();
+                user.setUsername(username);
+                user.setPassword(password);
+
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Intent intent = new Intent(getApplicationContext(), ListUsersActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                "There was an error signing up."
+                                    , Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
